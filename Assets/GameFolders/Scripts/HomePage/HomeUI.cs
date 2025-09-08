482fa0dccda49de6da721df250ea7c10ff932afa
Quách Thành Long <stephensouth1307@gmail.com>
2025-09-08 11:25:57 +0700

    - update

--- Assets/GameFolders/Scripts/HomePage/HomeUI.cs
+++ Assets/GameFolders/Scripts/HomePage/HomeUI.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class HomeUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private float fadeDuration = 3f;
+
+    public void StartGame()
     {
-        
+        StartCoroutine(FadeOutAndLoad());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator FadeOutAndLoad()
     {
-        
+        float timer = 0f;
+
+        while (timer < fadeDuration)
+        {
+            timer += Time.deltaTime;
+            canvasGroup.alpha = 1 - (timer / fadeDuration);
+            yield return null;
+        }
+
+        canvasGroup.alpha = 0;
+        SceneManager.LoadSceneAsync("Tutorial");
     }
 }
