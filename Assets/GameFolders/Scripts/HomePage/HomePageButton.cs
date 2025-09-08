3cfa1c1dea8b449644e2b39ee8161ec3c7931f1e
Quách Thành Long <stephensouth1307@gmail.com>
2025-09-08 10:53:56 +0700

    up

--- Assets/GameFolders/Scripts/HomePage/HomePageButton.cs
+++ Assets/GameFolders/Scripts/HomePage/HomePageButton.cs
@@ -1,33 +1,51 @@
 using UnityEngine;
-using System.Collections;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class HomePageButton : MonoBehaviour
 {
-    [SerializeField] private CanvasGroup canvasGroup;
-    [SerializeField] private float fadeDuration = 3f;
-    [SerializeField] private AudioClip homeMusic;
+    [SerializeField] private CanvasGroup canvasGroup;      // Assign in Inspector
+    [SerializeField] private float fadeDuration = 2f;       // Duration of fade
+    [SerializeField] private GameObject musicOptionsPanel;  // Assign your music options UI panel here
 
-
-    public void StartGame()
+    // Called when Tutorial button is clicked
+    public void OnTutorialButtonClicked()
     {
-        StartCoroutine(FadeOutAndLoad());
-      
-
+        StartCoroutine(FadeOutAndLoad("Tutorial"));
     }
 
-    private IEnumerator FadeOutAndLoad()
+    // Called when Music Options button is clicked
+    public void OnMusicOptionsButtonClicked()
+    {
+        if (musicOptionsPanel != null)
+        {
+            musicOptionsPanel.SetActive(true);
+        }
+        else
+        {
+            Debug.LogWarning("Music Options Panel not assigned in Inspector.");
+        }
+    }
+
+    private IEnumerator FadeOutAndLoad(string sceneName)
     {
         float timer = 0f;
 
         while (timer < fadeDuration)
         {
             timer += Time.deltaTime;
-            canvasGroup.alpha = 1 - (timer / fadeDuration);
+            if (canvasGroup != null)
+            {
+                canvasGroup.alpha = 1 - (timer / fadeDuration);
+            }
             yield return null;
         }
 
-        canvasGroup.alpha = 0;
-        SceneManager.LoadSceneAsync("Scene_1");
+        if (canvasGroup != null)
+        {
+            canvasGroup.alpha = 0;
+        }
+
+        SceneManager.LoadSceneAsync(sceneName);
     }
 }
